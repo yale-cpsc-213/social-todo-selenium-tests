780df01b67612039a7e8d595f827e4c2cebf2841
Kyle L. Jensen <kljensen@gmail.com>
2018-03-06 10:41:45 -0500

    Build without CGO

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ fi
 GOPATH=$HOME/go
 NAME="social-todo-selenium-tests"
 VERSION="$1"
-export GO_ENABLED=0
+export CGO_ENABLED=0
 GOOS=windows GOARCH=386 go build -o "$NAME-windows-$VERSION".exe
 GOOS=linux GOARCH=386 go build -o "$NAME-linux-$VERSION"
 go build -o "$NAME-mac-$VERSION"
